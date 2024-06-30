b6172069c8d28d688800da15e5e52f4cd680c058
SirImperivm <134490705+SirImperivm@users.noreply.github.com>
2024-06-30 19:54:19 +0200

    Changelog: - Inserito utilities per i colori e gli incantesimi; - Inserito sistema di logging automatico; - Inserita repostory Spigot per NMS.

--- src/main/java/me/sirimperivm/spigot/Main.java
+++ src/main/java/me/sirimperivm/spigot/Main.java
@@ -1,23 +1,48 @@
 package me.sirimperivm.spigot;
 
+import me.sirimperivm.spigot.utils.Logger;
+import me.sirimperivm.utilities.SirUtilities;
+import me.sirimperivm.utilities.colors.Colors;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("all")
 public final class Main extends JavaPlugin {
 
     private Main plugin;
+    private SirUtilities utilities;
+    private Colors colors;
+    private Logger log;
 
     @Override
     public void onEnable() {
         plugin = this;
+        utilities = new SirUtilities();
+        colors = utilities.getColors();
+        colors.setAcceptRGB(true);
+        colors.setBukkit(true);
+        log = new Logger(plugin, "RandomWorldGenerator");
+
+        log.success("Plugin attivato correttamente!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        log.success("Plugin disattivato correttamente!");
     }
 
     public Main getPlugin() {
         return plugin;
+    }
+
+    public Colors getColors() {
+        return colors;
+    }
+
+    public Logger getLog() {
+        return log;
+    }
+
+    public SirUtilities getUtilities() {
+        return utilities;
     }
 }
---
+++ src/main/java/me/sirimperivm/spigot/utils/Logger.java
@@ -0,0 +1,32 @@
+package me.sirimperivm.spigot.utils;
+
+import me.sirimperivm.spigot.Main;
+import me.sirimperivm.utilities.colors.Colors;
+
+@SuppressWarnings("all")
+public class Logger {
+
+    private Main plugin;
+    private Colors colors;
+
+    private String pluginName;
+
+    public Logger(Main plugin, String pluginName) {
+        this.plugin = plugin;
+        this.pluginName = pluginName;
+
+        colors = plugin.getColors();
+    }
+
+    public void success(String message) {
+        plugin.getServer().getConsoleSender().sendMessage(colors.translatedString("&2[" + pluginName + "] " + message));
+    }
+
+    public void info(String message) {
+        plugin.getServer().getConsoleSender().sendMessage(colors.translatedString("&e[" + pluginName + "] " + message));
+    }
+
+    public void fail(String message) {
+        plugin.getServer().getConsoleSender().sendMessage(colors.translatedString("&c[" + pluginName + "] " + message));
+    }
+}
